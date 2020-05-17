f55796ffbb0dc4ce7630f451c8edb2acbedc2df6
Dariusz <darek10121998db@gmail.com>
2020-05-17 20:38:38 +0200

    other layouts

--- image_coding_project/GUI_frame_decode_A.cpp
+++ image_coding_project/GUI_frame_decode_A.cpp
@@ -14,25 +14,30 @@ void GUI_frame_decode_A::frame_encode_A_OnUpdateUI( wxUpdateUIEvent& event )
 
 void GUI_frame_decode_A::button_load_input_OnButtonClick( wxCommandEvent& event )
 {
-    auto openFileDialog = std::make_unique<wxFileDialog>(this, _("Choose a image to load"), wxEmptyString,
-    wxEmptyString,_("Img files  (*.jpg)|*.jpg|Png files (*.png)|*.png"), wxFD_OPEN);
-    if (openFileDialog->ShowModal() == wxID_OK)
-    {
-        wxString fileName = openFileDialog->GetPath();
-    }
+auto openFileDialog = std::make_unique<wxFileDialog>(this, _("Choose a image to load"), wxEmptyString,
+wxEmptyString,_("Img files  (*.jpg)|*.jpg|Png files (*.png)|*.png"), wxFD_OPEN);
+if (openFileDialog->ShowModal() == wxID_OK)
+{
+wxString fileName = openFileDialog->GetPath();
+}
 }
 
 void GUI_frame_decode_A::button_encode_OnButtonClick( wxCommandEvent& event )
 {
-    
+
 }
 
 void GUI_frame_decode_A::button_save_output_OnButtonClick( wxCommandEvent& event )
 {
-    auto saveFileDialog = std::make_unique<wxFileDialog>(this, _("Choose a image to load"), wxEmptyString,
-    wxEmptyString,_("Img files  (*.jpg)|*.jpg|Png files (*.png)|*.png"), wxFD_SAVE | wxFD_OVERWRITE_PROMPT, wxDefaultPosition);
-    if (saveFileDialog->ShowModal() == wxID_OK)
-    {
-        wxString fileName = saveFileDialog->GetPath();
-    }
+auto saveFileDialog = std::make_unique<wxFileDialog>(this, _("Choose a image to load"), wxEmptyString,
+wxEmptyString,_("Img files  (*.jpg)|*.jpg|Png files (*.png)|*.png"), wxFD_SAVE | wxFD_OVERWRITE_PROMPT, wxDefaultPosition);
+if (saveFileDialog->ShowModal() == wxID_OK)
+{
+wxString fileName = saveFileDialog->GetPath();
+}
+}
+
+void GUI_frame_decode_A::button_return_OnButtonClick( wxCommandEvent& event )
+{
+// TODO: Implement button_return_OnButtonClick
 }
---
+++ image_coding_project/GUI_frame_encode_AB.cpp
@@ -0,0 +1,38 @@
+#include "GUI_frame_encode_AB.h"
+
+GUI_frame_encode_AB::GUI_frame_encode_AB( wxWindow* parent )
+:
+frame_encode_AB( parent )
+{
+
+}
+
+void GUI_frame_encode_AB::frame_encode_A_OnUpdateUI( wxUpdateUIEvent& event )
+{
+// TODO: Implement frame_encode_A_OnUpdateUI
+}
+
+void GUI_frame_encode_AB::button_load_input_OnButtonClick( wxCommandEvent& event )
+{
+// TODO: Implement button_load_input_OnButtonClick
+}
+
+void GUI_frame_encode_AB::button_encode_OnButtonClick( wxCommandEvent& event )
+{
+// TODO: Implement button_encode_OnButtonClick
+}
+
+void GUI_frame_encode_AB::button_save_output1_OnButtonClick( wxCommandEvent& event )
+{
+// TODO: Implement button_save_output1_OnButtonClick
+}
+
+void GUI_frame_encode_AB::button_save_output2_OnButtonClick( wxCommandEvent& event )
+{
+// TODO: Implement button_save_output2_OnButtonClick
+}
+
+void GUI_frame_encode_AB::button_return_OnButtonClick( wxCommandEvent& event )
+{
+// TODO: Implement button_return_OnButtonClick
+}
